da44bc3aacbbc1c5b3e8c8cb6d139ef6ab2afaaa
Rashmi Jain <84361823+Rashmijain18@users.noreply.github.com>
2025-06-11 17:50:08 -0700

    Worked on CSS of website for better experience

--- src/components/ContactInformation.tsx
+++ src/components/ContactInformation.tsx
@@ -3,29 +3,31 @@ import { FaLinkedin } from "react-icons/fa";
 
 export const ContactInformation = () => {
   return (
-    <div className="flex flex-col prose prose-invert">
-      <h2 className="text-lightest-slate">Get In Touch</h2>
-      <div className="flex flex-col gap-4">
-        <h3 className="text-green">Contact Me</h3>
-        <div className="flex gap-4">
-          <a
-            href="mailto:rashmi.jain24@outlook.com"
-            className="flex items-center gap-2 text-lightest-slate hover:text-green transition-colors"
-          >
-            <HiMail size={20} />
-            <span>rashmi.jain24@outlook.com</span>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/hirerashmi/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-lightest-slate hover:text-green transition-colors flex gap-2  items-center"
-          >
-            <FaLinkedin size={24} />
-            <span>LinkedIn</span>
-          </a>
-        </div>
+    <section
+      id="contact"
+      className="flex flex-col justify-center items-center "
+    >
+      <h3 className="text-lightest-slate">Get In Touch</h3>
+      <h3 className="text-green">Contact Me</h3>
+      <div className="flex gap-4">
+        <a
+          href="mailto:rashmi.jain24@outlook.com"
+          className="flex items-center gap-2 text-lightest-slate hover:text-green transition-colors"
+        >
+          <HiMail size={20} />
+          <span>rashmi.jain24@outlook.com</span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/hirerashmi/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-lightest-slate hover:text-green transition-colors flex gap-2 items-center"
+        >
+          <FaLinkedin size={24} />
+          <span>LinkedIn</span>
+        </a>
       </div>
-    </div>
+      <p className="mt-auto text-center p-4">Built by Rashmi Jain</p>
+    </section>
   );
 };
--- src/components/SideBar.tsx
+++
@@ -1,9 +0,0 @@
-export const SideBar = () => {
-  return (
-    <div className="p-6 prose prose-invert">
-      <h1 className="text-pink">Hi, my name is</h1>
-      <h2 className="text-lightest-slate">Rashmi.</h2>
-      <h2 className="text-lightest-slate">I build things for the web.</h2>
-    </div>
-  );
-};

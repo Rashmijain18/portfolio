f3bea628f7a110f5bf5038248c39b9a94eed20c0
Rashmi Jain <84361823+Rashmijain18@users.noreply.github.com>
2025-06-13 17:59:55 -0700

    Alignment corrected

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,11 +7,11 @@ import { Header } from "./Header";
 
 export const Home = () => {
   return (
-    <div className="min-h-screen bg-dark-navy text-slate w-full flex">
+    <div className="bg-dark-navy text-slate w-full flex">
       <div className="w-full flex flex-col">
         <Header />
-        <main className="flex justify-center items-center min-h-screen">
-          <div className="section-container w-[70vw] flex flex-col justify-center">
+        <main className="flex justify-center items-center">
+          <div className="section-container w-[70vw] flex flex-col gap-60">
             <Introduction />
             <About />
             <Skills />
